d71f0ffe3bc5c73fc18d1c77c4b284177151bdc7
Dominik Moritz <domoritz@gmail.com>
2016-03-02 10:26:07 -0800

    Add hello world

---
+++ himawari/himawari.go
@@ -0,0 +1,14 @@
+package himawari
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
