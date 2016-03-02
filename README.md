a7dfeb0fcd14ba5c9a0db37739a792455b6431d9
Dominik Moritz <domoritz@gmail.com>
2016-03-02 11:22:40 -0800

    Simple proxy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Himawari-8 JSON proxy
+
+This is a Google AppEngine application running at https://himawari-8.appspot.com/.
--- himawari/himawari.go
+++ himawari/himawari.go
@@ -1,14 +1,54 @@
 package himawari
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
+const BaseUrl = "http://himawari8-dl.nict.go.jp/himawari8/img/"
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, himawari!")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Content-Type", "text/json")
+
+	infrared := r.FormValue("infrared")
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString(BaseUrl)
+
+	if strings.HasPrefix(infrared, "t") {
+		buffer.WriteString("INFRARED_FULL")
+	} else {
+		buffer.WriteString("D531106")
+	}
+	buffer.WriteString("/latest.json")
+
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+	resp, err := client.Get(buffer.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// fmt.Fprintf(w, "HTTP GET returned status %v", resp.Status)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, string(body))
 }
