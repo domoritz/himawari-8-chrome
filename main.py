3a83c1bfd913a2c314c23d3cd7beeefc56629846
Dominik Moritz <domoritz@gmail.com>
2018-06-19 14:25:35 -0700

    Add caching

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
+import datetime
+import json
 import logging
 import re
-import json
-import datetime
 
-from google.appengine.api import urlfetch
+from google.appengine.api import memcache, urlfetch
 
 import webapp2
 
-
 URL = 'http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/'
-
+KEY = 'METEOSAT'
 
 class MainPage(webapp2.RequestHandler):
     def get_current_meteosat(self):
@@ -20,12 +19,12 @@ class MainPage(webapp2.RequestHandler):
             result = urlfetch.fetch(URL)
             if result.status_code == 200:
                 image_url = 'http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/IMAGESDisplay/' + re.search(r'array_nom_imagen\[0\]="(\w*)"', result.content).group(1)
-                date_string = re.search(r'\<option value="0"\>(.*)\<\/option\>').group(1)
-                # 19/06/18 15:00 UTC
-                date = datetime.datetime.strptime(date_string, "%d/%m/%y %H:%M UTC")
+                date_string = re.search(r'\<option value="0"\>(.*)\<\/option\>', result.content).group(1)
+                # 19/06/18 15:00 UTC  -> 2018-06-19 15:00:00
+                date = datetime.datetime.strptime(date_string, "%d/%m/%y %H:%M UTC").strftime('%Y-%m-%d %H:%M:%S')
 
                 return {
-                    'url': image_url
+                    'url': image_url,
                     'date': date
                 }
             else:
@@ -34,8 +33,13 @@ class MainPage(webapp2.RequestHandler):
             logging.exception('Caught exception fetching url')
 
     def get(self):
+        data = memcache.get(KEY)
+        if data is None:
+            data = json.dumps(self.get_current_meteosat())
+            memcache.add(KEY, data, 120)
+
         self.response.headers['Content-Type'] = 'text/json'
-        self.response.write(json.dumps(self.get_current_meteosat()))
+        self.response.write(data)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
