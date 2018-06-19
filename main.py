d5059dea59debb5ff1ce60df025ef78b66ce3044
Dominik Moritz <domoritz@gmail.com>
2018-06-19 10:15:34 -0700

    Parse dates

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 
 import logging
 import re
+import json
+import datetime
 
 from google.appengine.api import urlfetch
 
 import webapp2
 
 
-URL = "http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/"
+URL = 'http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/'
 
 
 class MainPage(webapp2.RequestHandler):
@@ -17,15 +19,23 @@ class MainPage(webapp2.RequestHandler):
         try:
             result = urlfetch.fetch(URL)
             if result.status_code == 200:
-                return re.search(r'array_nom_imagen\[0\]="(\w*)"', result.content).group(1)
+                image_url = 'http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/IMAGESDisplay/' + re.search(r'array_nom_imagen\[0\]="(\w*)"', result.content).group(1)
+                date_string = re.search(r'\<option value="0"\>(.*)\<\/option\>').group(1)
+                # 19/06/18 15:00 UTC
+                date = datetime.datetime.strptime(date_string, "%d/%m/%y %H:%M UTC")
+
+                return {
+                    'url': image_url
+                    'date': date
+                }
             else:
                 self.response.status_code = result.status_code
         except urlfetch.Error:
             logging.exception('Caught exception fetching url')
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write("http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/IMAGESDisplay/" + self.get_current_meteosat())
+        self.response.headers['Content-Type'] = 'text/json'
+        self.response.write(json.dumps(self.get_current_meteosat()))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
