9fbc03b9974e85d0fa8e9479af35fc059e36cb92
Dominik Moritz <domoritz@gmail.com>
2018-06-20 07:56:57 -0700

    Support 0 and 41.5 degrees.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 # meteosat-url
 
-Access at https://meteosat-url.appspot.com.
+Access at https://meteosat-url.appspot.com/msg (at 0 degrees) and https://meteosat-url.appspot.com/msgiodc (at 41.5 degrees).
--- main.py
+++ main.py
@@ -9,16 +9,14 @@ from google.appengine.api import memcache, urlfetch
 
 import webapp2
 
-URL = 'http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/'
-KEY = 'METEOSAT'
 
-class MainPage(webapp2.RequestHandler):
-    def get_current_meteosat(self):
+class Handler(webapp2.RequestHandler):
+    def get_current_meteosat(self, name):
         """Download current Meteosat 0 full disc view."""
         try:
-            result = urlfetch.fetch(URL)
+            result = urlfetch.fetch('http://oiswww.eumetsat.org/IPPS/html/' + name + '/RGB/NATURALCOLOR/FULLRESOLUTION/')
             if result.status_code == 200:
-                image_url = 'http://oiswww.eumetsat.org/IPPS/html/MSGIODC/RGB/NATURALCOLOR/FULLRESOLUTION/IMAGESDisplay/' + re.search(r'array_nom_imagen\[0\]="(\w*)"', result.content).group(1)
+                image_url = ('http://oiswww.eumetsat.org/IPPS/html/' + name + '/RGB/NATURALCOLOR/FULLRESOLUTION/IMAGESDisplay/') + re.search(r'array_nom_imagen\[0\]="(\w*)"', result.content).group(1)
                 date_string = re.search(r'\<option value="0"\>(.*)\<\/option\>', result.content).group(1)
                 # 19/06/18 15:00 UTC  -> 2018-06-19 15:00:00
                 date = datetime.datetime.strptime(date_string, "%d/%m/%y %H:%M UTC").strftime('%Y-%m-%d %H:%M:%S')
@@ -32,15 +30,27 @@ class MainPage(webapp2.RequestHandler):
         except urlfetch.Error:
             logging.exception('Caught exception fetching url')
 
-    def get(self):
-        data = memcache.get(KEY)
+    def get_with_name(self, name):
+        data = memcache.get(name)
         if data is None:
-            data = json.dumps(self.get_current_meteosat())
-            memcache.add(KEY, data, 120)
+            data = json.dumps(self.get_current_meteosat(name))
+            memcache.add(name, data, 120)
 
         self.response.headers['Content-Type'] = 'text/json'
         self.response.write(data)
 
+class MSGIODCHandler(Handler):
+    ''' At 41.5 Degree '''
+    def get(self):
+        return self.get_with_name('MSGIODC')
+
+
+class MSGHandler(Handler):
+    ''' At 0 Degree '''
+    def get(self):
+        return self.get_with_name('MSG')
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/msgiodc', MSGIODCHandler),
+    ('/msg', MSGHandler),
 ], debug=True)
