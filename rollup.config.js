c755ccb60a9f8b678f39f96da5831d98f2c47167
Dominik Moritz <domoritz@gmail.com>
2020-06-11 12:08:49 -0700

    chore: upgrade deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import nodeResolve from "rollup-plugin-node-resolve";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 
